bd0aebf7253de6b7921d14e00f54981ee6fe3faa
iszmxw <mail@54zm.com>
2021-05-03 00:30:48 +0800

    修改main

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"gorbac/bootstrap"
 	"gorbac/config"
+	conf "gorbac/pkg/config"
 )
 
 func init() {
@@ -10,5 +13,10 @@ func init() {
 }
 
 func main() {
-
+	// 初始化 SQL
+	bootstrap.SetupDB()
+	// 初始化路由绑定
+	router := bootstrap.SetupRoute()
+	// 启动路由
+	_ = router.Run(fmt.Sprintf(":%s", conf.GetString("app.port")))
 }
